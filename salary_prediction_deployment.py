7d6e3febf29a5764f7ae4991fc0d33064c2ed366
Suraj yadav <surajfci335@gmail.com>
2026-08-19 08:13:15 +0300

    Fix syntax error in salary prediction deployment

--- salary_prediction_deployment.py
+++ salary_prediction_deployment.py
@@ -35,6 +35,6 @@ df =pd.DataFrame({
 
 if st.button("Predict Salary"):
   for col in encoder:
-    df(col) = encoder[col].transform(df[col])
+    df[col] = encoder[col].transform(df[col])
   prediction =model.predict(df)
   st.success(f"The predicted salary is :  {prediction[0]:,.2f}")
